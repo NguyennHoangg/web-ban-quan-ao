44ed0530bba4d73772bb9540034caed5c1c07936
Nguyễn Huy Hoàng <139753465+NguyennHoangg@users.noreply.github.com>
2026-04-23 14:31:50 +0700

    Update cors.js

--- BE/src/config/cors.js
+++ BE/src/config/cors.js
@@ -6,7 +6,7 @@
 require('dotenv').config();
 
 module.exports = {
-  origin: process.env.CLIENT_URL,
+   origin: process.env.CLIENT_URL || 'http://localhost:5173' || 'https://web-ban-quan-ao-nu.vercel.app/',
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
