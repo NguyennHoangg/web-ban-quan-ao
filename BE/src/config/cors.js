36784a7c2d0c9c66837e2fd28b249f300f2c557e
NguyennHoangg <nguyenhuyhoang270804@gmail.com>
2026-04-26 17:08:58 +0700

    Update BE: gitignore, schema & yame_import, product service/model/routes/controller, cors & db config

--- BE/src/config/cors.js
+++ BE/src/config/cors.js
@@ -6,7 +6,12 @@
 require('dotenv').config();
 
 module.exports = {
-  origin: process.env.CLIENT_URL,
+  origin: [
+    process.env.CLIENT_URL,
+    'http://localhost:5173',
+    'http://localhost:3000',
+    'https://web-ban-quan-ao-nu.vercel.app'
+  ].filter(Boolean),
   methods: ["GET", "POST", "PUT", "DELETE"],
   credentials: true,
 };
